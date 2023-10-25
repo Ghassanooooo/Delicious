1310bfc747fbe18ca1fb85716d653100b4e4aad0
Ghassanooooo <ghassanaldarwish@yahoo.com>
2023-10-25 15:10:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# what do you know about hooks?
+
+# 1. What is a hook?
+
+# useEffect is a hook that lets you perform side effects in function components
+
+# the component same as creatures have life cycles, birth(mounted), growth(update), death(unmounted)
+
+![](https://miro.medium.com/v2/resize:fit:900/1*LPPZd97F-0-YWlZzDhNgVw.png)
+
+# useEffect is the most important hook in react, because it lets you control how the component behaves in each cycle
+
+## the GOD hook
---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import "./App.css";
+
+import { useEffect, useRef, useState } from "react";
+
+export default function App() {
+  const [list, setList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [meals, setMeals] = useState([]);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    async function getIngredient() {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
+      );
+      const data = await response.json();
+
+      const ingredients = data.meals.map((ingredient) => {
+        return ingredient.strIngredient.trim().toLowerCase();
+      });
+      setList(ingredients);
+    }
+    try {
+      getIngredient();
+    } catch (err) {
+      alert(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    async function getIngredient() {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + searchQuery
+      );
+      const data = await response.json();
+
+      setMeals(data.meals || []);
+    }
+
+    try {
+      getIngredient();
+    } catch (err) {
+      alert(err);
+    }
+  }, [searchQuery]);
+
+  console.log(meals);
+
+  const handerSearch = (e) => {
+    const value = e.target.value.trim().toLowerCase();
+    if (!value || value.length < 3) {
+      setIngredients([]);
+      return;
+    }
+    const find = list.filter((str) => str.includes(value));
+    setIngredients(find);
+  };
+
+  const handlerSelectIngredient = (e) => {
+    const value = e.target.textContent;
+    setIngredients([]);
+    setSearchQuery(value);
+    inputRef.current.value = value;
+  };
+
+  console.log(searchQuery);
+  return (
+    <div>
+      <h1 className="text-3xl text-center p-8">Delicious</h1>
+      <div className="w-1/2 m-auto mt-8 relative">
+        <input
+          placeholder="Search by ingredient"
+          ref={inputRef}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          onChange={handerSearch}
+        />
+        {ingredients.length >= 3 && (
+          <div className="border p-4 absolute top-10 w-full bg-slate-50">
+            {ingredients.map((ingredient) => (
+              <p
+                onClick={handlerSelectIngredient}
+                className="hover:underline cursor-pointer"
+                key={ingredient}
+              >
+                {ingredient}
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
+      {meals.length > 0 ? (
+        <div className="w-[80%] m-auto mt-8 grid grid-cols-5 gap-4">
+          {meals.map((meal) => (
+            <div
+              className=" rounded overflow-hidden shadow-lg "
+              key={meal.idMeal}
+            >
+              <img className="w-full" src={meal.strMealThumb} alt="Mountain" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{meal.strMeal}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="text-center p-10">No Meals Found</div>
+      )}
+    </div>
+  );
+}
